a259f380ca3eb347d1ffe6dc99b8eb9cea41d351
Wahab Bello <wahaaabello@gmail.com>
2019-08-04 00:43:42 +0100

    initial commit

---
+++ android/app/src/main/java/com/doksta_rn/MainActivity.java
@@ -0,0 +1,15 @@
+package com.doksta_rn;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "doksta_rn";
+    }
+}
